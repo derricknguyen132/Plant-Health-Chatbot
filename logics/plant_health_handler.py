a6d1fe15cdafa123ebf1276daaa7da4bda5c760d
derricknguyen132 <pogo132655@gmail.com>
2024-10-26 00:51:10 +0800

    Second commit

--- logics/plant_health_handler.py
+++ logics/plant_health_handler.py
@@ -1,27 +1,5 @@
-import pandas as pd
-import openai
-from sentence_transformers import SentenceTransformer, util
-import re
-from transformers import pipeline
-
-# Initialize OpenAI API
-openai.api_key = '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'  # Replace with your actual API key
-
-
-# Load environment variables
-from dotenv import load_dotenv
-
-# Load your .env file
-load_dotenv()
-
-# Load the questions and answers from the CSV file
-df = pd.read_csv('./data/FAQ.csv')
-
-# Initialize SentenceTransformer model
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
 # Main chatbot function that integrates all components
-def chatbot_response(prompt):
+def chatbot_response(prompt, df, model):
     if not is_prompt_relevant(prompt):
         return "I'm sorry, it appears I have trouble understanding your request or your request is out of my scope. Please consider rephrasing, elaborating or changing your question."
 
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
+import openai
+from sentence_transformers import SentenceTransformer
+from dotenv import load_dotenv
+import os
 from logics.plant_health_handler import chatbot_response
+from utility import check_password
 
 # Streamlit App Configuration
 st.set_page_config(
@@ -10,13 +15,32 @@ st.set_page_config(
 
 st.title("Plant Health Chatbot")
 
+# Do not continue if check_password is not True.  
+if not check_password():  
+    st.stop()
+
+# Load environment variables once
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+# Load the questions and answers from the CSV file once
+df = pd.read_csv('./data/FAQ.csv', encoding='utf-8')
+
+# Initialize SentenceTransformer model once
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+
+
+# Create input form for user query
 form = st.form(key="form")
 form.subheader("Ask your gardening question")
 
 user_prompt = form.text_area("Enter your question here", height=200)
 
+# Process the user's input upon submission
 if form.form_submit_button("Submit"):
     st.toast(f"User Input Submitted - {user_prompt}")
-
-    response = chatbot_response(user_prompt)
+    
+    # Pass the initialized `df` and `model` to the chatbot logic
+    response = chatbot_response(user_prompt, df, model)
     st.write(response)
