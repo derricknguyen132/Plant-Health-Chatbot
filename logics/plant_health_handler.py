39ebfac4c52506717c14c60ea899bd91de95a3c2
lnwanderer <derrick_nguyen@nparks.gov.sg>
2024-10-28 16:21:29 +0800

    Update plant_health_handler.py

--- logics/plant_health_handler.py
+++ logics/plant_health_handler.py
@@ -6,10 +6,10 @@ def chatbot_response(prompt, df, model):
 
     if combined_answers:
         final_answer = synthesize_final_answer(prompt, combined_answers)
-        response_source = "synthesize_final_answer"
+        response_source = "database"
     else:
         final_answer = "*Disclaimer: The following response may not be accurate as your question was not found in the verified database.*"
         final_answer += f'\n\n{generate_self_response(prompt)}'
-        response_source = "generate_self_response"
+        response_source = "self_generate"
 
     return final_answer, response_source
